a3259f01f09e3639018973fb5f30798447771e28
Francesco Tarchi <francesco.tarchi01@gmail.com>
2025-03-13 13:34:51 +0100

    Aggiornati file per l'inserimento delle ultime 3 (se ci sono) recensioni in ogni annata di ogni vino.

--- dataset/vivino_scraper/sort_reviews.py
+++ dataset/vivino_scraper/sort_reviews.py
@@ -2,11 +2,16 @@
 
 import json
 
-# leggo il file reviews.json
-with open('json/reviews.json', 'r', encoding='utf-8') as file:
-    reviews = json.load(file)
+# Apro i file
+with open(r'C:\Users\franc\OneDrive - University of Pisa\Documenti\_Progetti magistrale\Large\datasets\reviews.json', encoding='utf-8') as f:
+    reviews = json.load(f)
+f.close()
 
-reviews_sorted = sorted(reviews, key=lambda x: (x['wine_id'] , x['created_at']))
+with open(r'C:\Users\franc\OneDrive - University of Pisa\Documenti\_Progetti magistrale\Large\datasets\users.json', encoding='utf-8') as f:
+    users = json.load(f)
+f.close()
+
+reviews_sorted = sorted(reviews, key=lambda x: (x['user_id']['username'], x['created_at']))
 
 # scrivo il file reviews_sorted.json
 with open('json/reviews_sorted.json', 'w', encoding='utf-8') as file:
