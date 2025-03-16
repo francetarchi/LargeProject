ba67848d6b6e3e0f1dbcdf562c753be09c59952c
Valentina Bertei <vbertei.vb@gmail.com>
2025-03-16 18:02:58 +0100

    Sistemate annotazioni nelle classi e cambiata la dipendenza per le repository.

--- WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/model/Review.java
+++ WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/model/Review.java
@@ -1,12 +1,17 @@
 package com.wineadvisor.wineadvisor.model;
 
 import org.bson.types.ObjectId;
-import lombok.Data;
 
-import java.util.*;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
 
 @Data
+@Document(collection = "reviews")
 public class Review {
+    @Id
     private ObjectId id;
     private UserInfo user;
     private WineInfo wine;
--- WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/repository/ReviewRepository.java
+++ WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/repository/ReviewRepository.java
@@ -1,8 +1,9 @@
 package com.wineadvisor.wineadvisor.repository;
 
 import com.wineadvisor.wineadvisor.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-    
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, Long> {    
 }
