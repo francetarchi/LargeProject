8816a3f0fc68aacd84123aa825a2338f2f77cccd
Valentina Bertei <vbertei.vb@gmail.com>
2025-03-16 17:29:24 +0100

    Tolti setters e getters inutili dalle recensioni.

--- WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/model/Review.java
+++ WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/model/Review.java
@@ -1,12 +1,12 @@
 package com.wineadvisor.wineadvisor.model;
 
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
+import lombok.Data;
 
 import java.util.*;
 
+@Data
 public class Review {
-    @Id
     private ObjectId id;
     private UserInfo user;
     private WineInfo wine;
@@ -15,81 +15,4 @@ public class Review {
     private Date createdAt;
     private int likesCount;
     private int dislikesCount;
-
-    public Review() {}
-
-    public Review(UserInfo user, WineInfo wine, double rating, String text, Date createdAt, int likesCount, int dislikesCount) {
-        this.user = user;
-        this.wine = wine;
-        this.rating = rating;
-        this.text = text;
-        this.createdAt = createdAt;
-        this.likesCount = likesCount;
-        this.dislikesCount = dislikesCount;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public UserInfo getUser() {
-        return user;
-    }
-
-    public WineInfo getWine() {
-        return wine;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public int getLikesCount() {
-        return likesCount;
-    }
-
-    public int getDislikesCount() {
-        return dislikesCount;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public void setUser(UserInfo user) {
-        this.user = user;
-    }
-
-    public void setWine(WineInfo wine) {
-        this.wine = wine;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public void setLikesCount(int likesCount) {
-        this.likesCount = likesCount;
-    }
-
-    public void setDislikesCount(int dislikesCount) {
-        this.dislikesCount = dislikesCount;
-    }
-
 }
--- WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/model/UserInfo.java
+++ WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/model/UserInfo.java
@@ -1,29 +1,9 @@
 package com.wineadvisor.wineadvisor.model;
 
+import lombok.Data;
+
+@Data
 public class UserInfo {
     private String username;
     private String thumbnail;
-
-    public UserInfo() {}
-    
-    public UserInfo(String username, String thumbnail) {
-        this.username = username;
-        this.thumbnail = thumbnail;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getThumbnail() {
-        return thumbnail;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setThumbnail(String thumbnail) {
-        this.thumbnail = thumbnail;
-    }
 }
