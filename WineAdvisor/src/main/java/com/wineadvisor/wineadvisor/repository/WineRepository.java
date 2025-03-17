827a60345ac252e657ab41934458d9bbb53f61cf
Francesco Tarchi <92877871+francetarchi@users.noreply.github.com>
2025-03-17 16:20:59 +0100

    Update WineRepository.java

--- WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/repository/WineRepository.java
+++ WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/repository/WineRepository.java
@@ -1,7 +1,9 @@
 package com.wineadvisor.wineadvisor.repository;
 
 import com.wineadvisor.wineadvisor.model.Wine;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface WineRepository extends JpaRepository<Wine, Long> {
+@Repository
+public interface WineRepository extends MongoRepository<Wine, Long> {
 }
