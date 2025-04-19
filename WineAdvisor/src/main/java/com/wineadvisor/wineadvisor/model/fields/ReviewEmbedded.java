e1cce1d1e7258812d37d85ad94802597f2ab68c0
Francesco Tarchi <francesco.tarchi01@gmail.com>
2025-04-19 20:36:15 +0200

    Updated ReviewEmbedded - Scordato di includerlo nel commit precedente.

--- WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/model/fields/ReviewEmbedded.java
+++ WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/model/fields/ReviewEmbedded.java
@@ -7,7 +7,6 @@ import org.springframework.data.mongodb.core.mapping.Field;
 import com.wineadvisor.wineadvisor.model.fields.reviews.UserId;
 import com.wineadvisor.wineadvisor.model.fields.reviews.WineId;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,21 +25,16 @@ public class ReviewEmbedded {
     @Field("wine_id")
     private WineId wineId;
     
-    @Schema(description = "rating", example = "4.5")
     private Double rating;
 
-    @Schema(description = "text", example = "This wine is amazing!")
     private String text;
 
     @Field("created_at")
-    @Schema(description = "createdAt", example = "2023-10-01T12:00:00.000Z")
     private LocalDateTime createdAt;
 
     @Field("likes_count")
-    @Schema(description = "likesCount", example = "10")
     private Long likesCount;
 
     @Field("dislikes_count")
-    @Schema(description = "dislikesCount", example = "2")
     private Long dislikesCount;
 }
