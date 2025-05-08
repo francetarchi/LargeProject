b9b0056b27dc1225630beee2f94683d7893daee7
smartisssssssssss <m.bertela@studenti.unipi.it>
2025-05-08 15:40:37 +0200

    aggiunto import

--- WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/config/SecurityConfig.java
+++ WineAdvisor/src/main/java/com/wineadvisor/wineadvisor/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.wineadvisor.wineadvisor.config;
 
+import org.springframework.security.config.Customizer; //TEST
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
